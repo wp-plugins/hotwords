c5e4afcbece7ddab9c8e2d36c73ad683638440dd
Anderson.Makiyama <Anderson.Makiyama@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-02 22:49:18 +0000

    Correção de alguns links, coloquei screenshots na pasta assets, conforme o novo padrão.
    git-svn-id: https://plugins.svn.wordpress.org/hotwords/tags/0.2@707196 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/help.php
@@ -0,0 +1,86 @@
+<div class="wrap">
+<div class="icon32"><img src='<?php echo plugins_url('/images/icon-32.png', dirname(__FILE__))?>' /></div>
+        
+<h2>Ajuda <?php echo self::PLUGIN_NAME?>:</h2>
+
+
+<p>Para obter Ajuda e mais informações, visite a página do Plugin</p>
+    
+  		<table width="100%"><tr>
+        <td style="vertical-align:top;">
+        
+        <div class="metabox-holder">
+		<div class="postbox" >
+        
+        <p>
+        Para obter ajuda e mais detalhes, acesse a página do Plugin: <a href="<?php echo self::PLUGIN_PAGE?>" target="_blank"><?php echo self::PLUGIN_PAGE?></a>
+        </p>
+        
+        	<h3 style="font-size:24px; text-transform:uppercase;color:#F00;">
+        	Gostou do Plugin? Curta os meus Sites!
+            </h3>
+            
+             <h3><a href="http://fazer-site.net" target="_blank">www.Fazer-Site.Net</a></h3>
+             
+        	<div class="inside">
+                <p>
+                <iframe src="<?php echo $anderson_makiyama[self::PLUGIN_ID]->plugin_url?>author/profissionais2.html" width="1000" height="280" frameborder="0" scrolling="no"></iframe>
+				</p>
+
+			</div>
+            
+           <h3><a href="http://ganhardinheiroblog.net" target="_blank">www.GanharDinheiroBlog.Net</a></h3>
+            
+        	<div class="inside">
+                <p>
+                  <iframe src="<?php echo $anderson_makiyama[self::PLUGIN_ID]->plugin_url?>author/ganhardinheiroblog2.html" width="1000" height="280" frameborder="0" scrolling="no"></iframe>
+                </p>
+
+			</div>        
+            
+                        
+		</div>
+        </div>
+              
+       </td>
+       </tr>
+       </table>
+
+
+<hr />
+
+
+
+<table>
+<tr>
+<td>
+<img src="<?php echo $anderson_makiyama[self::PLUGIN_ID]->plugin_url?>images/anderson-makiyama.png" />
+</td>
+<td>
+<ul>
+
+<li>Autor: <strong>Anderson Makiyama</strong>
+
+</li>
+
+<li>Email do Autor: <a href="mailto:andersonmaki@gmail.com" target="_blank">andersonmaki@gmail.com</a>
+
+</li>
+
+<li>Visite a Página do Plugin: <a href="<?php echo self::PLUGIN_PAGE?>" target="_blank"><?php echo self::PLUGIN_PAGE?></a>
+
+</li>
+
+<li>
+
+Visite a Página do Autor: <a href="http://fazer-site.net" target="_blank">Fazer Site</a>
+
+</li>
+
+</ul>
+</td>
+</tr>
+</table>
+
+
+</div>
